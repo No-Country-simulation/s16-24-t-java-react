32389f0e0357c448b9b0fdf3439918313e2aa731
Rafael Toresani <rtoresani@gmail.com>
2024-07-09 16:56:30 -0300

    add: repositories

--- server/ManagementAPI/src/main/java/com/project/managementapi/repositories/MembershipRepository.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/repositories/MembershipRepository.java
@@ -1,10 +1,9 @@
 package com.project.managementapi.repositories;
 
-import com.project.managementapi.entities.userEntity.UserEntity;
+import com.project.managementapi.entities.membership.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+public interface MembershipRepository extends JpaRepository<Membership, Long> {
 }
--- server/ManagementAPI/src/main/java/com/project/managementapi/repositories/PersonalInfoRepository.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/repositories/PersonalInfoRepository.java
@@ -1,10 +1,9 @@
 package com.project.managementapi.repositories;
 
-import com.project.managementapi.entities.userEntity.UserEntity;
+import com.project.managementapi.entities.personalInfo.PersonalInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+public interface PersonalInfoRepository extends JpaRepository<PersonalInfo, Long> {
 }
