4d8338e9023ed336a32efa30949e6fc63401f8da
Rafael Toresani <rtoresani@gmail.com>
2024-07-09 15:23:08 -0300

    add: annotations userEntity

--- server/ManagementAPI/src/main/java/com/project/managementapi/entities/userEntity/UserEntity.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/entities/userEntity/UserEntity.java
@@ -1,8 +1,15 @@
 package com.project.managementapi.entities.userEntity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
-
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Users")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
