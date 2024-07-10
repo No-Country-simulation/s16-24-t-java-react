d695be748706a5cc208ff04e1075c0a2e3488bc4
PHRCOOK <https://pabloelleproso@gmail.com>
2024-07-09 22:02:56 -0300

    config tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./src/**/*.{html,js,jsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
