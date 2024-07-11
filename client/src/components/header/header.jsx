f828297057a30a0cae8598638f70a965ea495b13
PHRCOOK <https://pabloelleproso@gmail.com>
2024-07-10 21:19:00 -0300

    sidebar modification

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import logo from "../.././../src/assets/OIG2.jpeg";
 
 function Header() {
 	return (
 		<header className="bg-gray-800 text-white shadow-md py-4">
-			<div className="container mx-auto px-4">
-				<h1 className="text-xl font-bold">Header</h1>
+			<div className="container mx-auto px-4 flex items-center justify-between">
+				{/* Logo a la izquierda */}
+				<div className="flex items-center">
+					<img
+						src={logo} // Reemplaza con la URL o ruta de tu logo
+						alt="Logo de la empresa"
+						className="h-20 mr-2" // Clase para ajustar el tamaño del logo
+					/>
+					<h1 className="text-xl font-bold">Sportify</h1>{" "}
+					{/* Nombre en el centro */}
+				</div>
+
+				{/* Botón de login a la derecha */}
+				<button className="bg-white text-gray-800 font-semibold py-2 px-4 rounded">
+					Login
+				</button>
 			</div>
 		</header>
 	);
