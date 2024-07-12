984218b1cb54d7e9fe813659b667e88e1c2fac34
pablo lopez <pablo_98_18@hotmail.com>
2024-07-12 20:23:17 -0300

    Component removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Home from "../src/components/home/home.jsx"; // Importa el componente hom
 import axios from "axios";
 import "./App.css";
 
-import CreateUser from "./components/modals/createUser.jsx";
+import CreateUser from "./components/Modals/createUser.jsx";
 
 axios.defaults.baseURL = "";
 
