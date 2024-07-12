c9f8b0c096aaa4abba72e2f2b6d50c09ddae0a2c
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-12 15:56:50 -0300

    New: buttons for add user and generate report

---
+++ client/src/components/table/report-button.jsx
@@ -0,0 +1,11 @@
+import Icon from "../accesories/icon"
+
+function ReportButton() {
+  return (
+    <button className="bg-secondary-0 text-pirmary-0 px-2 py-1">
+      <Icon iconName="downloadFile"/>
+    </button>
+  )
+}
+
+export default ReportButton
