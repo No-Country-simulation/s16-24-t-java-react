80e6b84799da14b9f94ddaa78abe2cd33b538526
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-16 13:03:46 -0300

    Fix: remove, unuse component

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -9,7 +9,6 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Typography from "@mui/material/Typography";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { styled, useTheme } from "@mui/material/styles";
