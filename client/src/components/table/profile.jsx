3356c5f6ef522c15778ddbe9767b92dda6137821
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-16 10:07:30 -0300

    New: Profile Button

---
+++ client/src/components/table/profile.jsx
@@ -0,0 +1,11 @@
+import Icon from "../accesories/icon"
+
+function ProfileButton () {
+  return (
+    <button className="bg-primary-0 text-secondary-0 px-2 py-1">
+      <Icon iconName="profile" />
+    </button>
+  )
+}
+
+export default ProfileButton
