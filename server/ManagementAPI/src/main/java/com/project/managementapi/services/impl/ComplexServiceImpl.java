0daa079b66c5e986dbe75105286b2f985320c4c7
Rafael Toresani <rtoresani@gmail.com>
2024-07-17 16:08:09 -0300

    add: advanced search complex implemented

--- server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/ComplexServiceImpl.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/ComplexServiceImpl.java
@@ -1,15 +1,23 @@
 package com.project.managementapi.services.impl;
 
 import com.project.managementapi.dtos.ComplexDTO;
+import com.project.managementapi.dtos.EmployeeDTO;
 import com.project.managementapi.entities.Complex;
 import com.project.managementapi.exceptions.ResourceAlreadyExistsException;
 import com.project.managementapi.repositories.ComplexRepository;
 import com.project.managementapi.services.IComplexService;
+import com.project.managementapi.specifications.ComplexSpecification;
 import com.project.managementapi.utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class ComplexServiceImpl implements IComplexService {
@@ -38,4 +46,23 @@ public class ComplexServiceImpl implements IComplexService {
     }
 
 
+    public Page<ComplexDTO> findComplexes(
+            String cuit,
+            String title,
+            Pageable pageable
+    ){
+        Specification<Complex> spec = Specification
+                .where(ComplexSpecification.hasTitle(title)
+                .and(ComplexSpecification.hasCuit(cuit)));
+
+        Page<Complex> complexes = complexRepository.findAll(spec, pageable);
+
+        List<ComplexDTO> complexDTOs = new ArrayList<>();
+
+        for(Complex c: complexes){
+            complexDTOs.add(Mapper.complexToDTO(c));
+        }
+
+        return new PageImpl<>(complexDTOs, pageable, complexes.getTotalElements());
+    }
 }
