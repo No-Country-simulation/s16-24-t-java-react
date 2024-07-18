159ec9d54092650049a41dc326ce9c65266bcc37
MQuirico <128657405+MQuirico@users.noreply.github.com>
2024-07-18 19:34:53 -0300

    Context for users

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,6 +1,7 @@
 import Sidebar from "../sidebar/sidebar.jsx";
 import Footer from "../footer/footer.jsx";
 import Table from "../table/table.jsx";
+import { UsersProvider } from "../table/usersInfoContext.jsx";
 
 function Home({ handleLogOut }) {
 	return (
@@ -8,7 +9,9 @@ function Home({ handleLogOut }) {
 			<div className="flex flex-1 bg-gray-200">
 				<Sidebar />
 				<main className="h-full w-full px-10">
-					<Table handleLogOut={handleLogOut} />
+				<UsersProvider>
+				<Table handleLogOut={handleLogOut} />
+				</UsersProvider>
 				</main>
 			</div>
 			<Footer />
--- client/src/components/table/table-row.jsx
+++ client/src/components/table/table-row.jsx
@@ -1,7 +1,19 @@
-function TableRow ({user, handleProfile}) {
+import * as React from "react"
+import { usersContext } from "./usersInfoContext"
+function TableRow ({user}) {
+
+const {profile, setProfile, chosenUser, setChosenUser} = React.useContext(usersContext)
+
+const handleProfile = (userID) => {
+  setChosenUser(userID)
+  console.log(chosenUser)
+  setProfile(!profile)
+  console.log(profile)
+}
+
   return (
     <tr className="text-center [&>td]:py-4  [&>td]:border-b [&>td]:border-primary-0">
-      <td className="border-l cursor-pointer" onClick={() => handleProfile(user)}>{user.nombreCompleto}</td>
+      <td className="border-l cursor-pointer" onClick={() => handleProfile(user.dni)}>{user.nombreCompleto}</td>
       <td>{user.fechaNacimiento}</td>
       <td>{user.dni}</td>
       <td>{user.deporte}</td>
