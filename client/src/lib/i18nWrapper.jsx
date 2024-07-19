c7c79b417fad769ed88bc309637624ae7dc3c3ea
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-19 17:08:16 -0300

    Fix: Supens fallback for app

--- client/src/lib/i18nWrapper.jsx
+++ client/src/lib/i18nWrapper.jsx
@@ -1,7 +1,8 @@
 import { Suspense } from "react";
+import AppSkeleton from "../components/skeletons/app-skeleton";
 
 function I18nWrapper({ children }) {
-  return <Suspense fallback={'...is loading'}>{children}</Suspense>;
+  return <Suspense fallback={<AppSkeleton />}>{children}</Suspense>;
 }
 
 export default I18nWrapper
