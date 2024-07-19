bc1f381174a68410ba597732bcef15ccfe043797
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-19 10:16:33 -0300

    Add: context to serve pathname

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -1,14 +1,17 @@
 import Sidebar from "../sidebar/sidebar.jsx";
 import Footer from "../footer/footer.jsx";
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function Home() {
+
+	const { pathname } = useLocation();
 	return (
 		<>
 			<div className="flex flex-1 bg-gray-200 pt-10">
 				<Sidebar />
 				<main className="h-full w-full px-10">
-					<Outlet />
+					<Outlet context={pathname}/>
 				</main>
 			</div>
 			<Footer />
