899b49486232269817d4bc0bfca95e747bc635f8
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-19 14:18:14 -0300

    Fix: change to Login context logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,32 @@
-import Home from "../src/components/home/home.jsx"; // Importa el componente home
+import { useEffect, useContext } from "react";
+import { LoginContext } from "./contexts/login-context.jsx";
+
+import Home from "../src/components/home/home.jsx"; 
+import Login from "./components/login/login.jsx";
+
 import axios from "axios";
 
-import { useState, useEffect } from "react";
 
-import Login from "./components/login/login.jsx";
 
 axios.defaults.baseURL = "https://sportify-xric.onrender.com";
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const { isLogin, setIsLogin } = useContext(LoginContext);
 
 	useEffect(() => {
 		// Verificar si hay una sesión almacenada en localStorage al cargar la aplicación
 		const loggedInStatus = localStorage.getItem("sportify_jwt_access");
 		if (loggedInStatus) {
-			setIsLoggedIn(true);
+			setIsLogin(true);
 		}
-	}, []); // Solo se ejecuta una vez al montar el componente
-
-
-	const handleLogout = () => {
-		setIsLoggedIn(false);
-		// Limpiar estado de sesión en localStorage
-		localStorage.removeItem("sportify_jwt_access");
-	};
+	}, []);
 
 	return (
 		<div className="App min-h-dvh flex flex-col max-h-dvh">
-			{isLoggedIn ? (
-				<Home handleLogOut={handleLogout} />
+			{isLogin ? (
+				<Home />
 			) : (
-				<Login setIsLoggedIn={setIsLoggedIn} />
+				<Login />
 			)}
 		</div>
 	);
