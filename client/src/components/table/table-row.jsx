b8bb09f4a3e13f3f324dd77c1f6e41c56a3079d3
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-19 10:13:08 -0300

    Fix: make re-usable

--- client/src/components/table/table-row.jsx
+++ client/src/components/table/table-row.jsx
@@ -1,27 +1,14 @@
 function TableRow ({user}) {
   return (
-    <tr className="text-center [&>td]:py-4  [&>td]:border-b [&>td]:border-primary-0">
-      <td className="border-l">{user.nombreCompleto}</td>
-      <td>{user.fechaNacimiento}</td>
-      <td>{user.dni}</td>
-      <td>{user.deporte}</td>
-      <td>{user.tipoMembresia}</td>
-      <td>{user.tipoCuota}</td>
-      <td>{user.fechaVencimientoCuota}</td>
-      <td>{user.diasDesdeVencimiento}</td>
-      <td className="border-r">{user.fechaAlta}</td>
+    <tr className="text-center [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0">
+      {Object.keys(user).map((key, index) => (
+        <td key={key} className={`custom-td ${index === 0 ? 'first:border-l' : ''} ${index === Object.keys(user).length - 1 ? 'last:border-r' : ''}`}>
+          {user[key]}
+        </td>
+      ))}
     </tr>
   )
 }
 
 export default TableRow
 
-// nombreCompleto: string;
-//     fechaNacimiento: string;
-//     dni: string;
-//     deporte: string;
-//     tipoMembresia: string;
-//     tipoCuota: string;
-//     fechaVencimientoCuota: string;
-//     diasDesdeVencimiento: number;
-//     fechaAlta: string;
