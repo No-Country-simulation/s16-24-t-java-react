2a7b80147cbe826f6ec7e98be87550e8aa456423
Leonel Amaya <omar.mejia2099@gmail.com>
2024-07-20 11:47:46 -0500

    add: logic for memberships persistence

--- server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/CustomerServiceImpl.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.project.managementapi.services.impl;
 
 import com.project.managementapi.dtos.CustomerDTO;
+import com.project.managementapi.dtos.MembershipDTO;
 import com.project.managementapi.entities.Customer;
 import com.project.managementapi.exceptions.ResourceNotFoundException;
 import com.project.managementapi.repositories.CustomerRepository;
 import com.project.managementapi.services.ICustomerService;
+import com.project.managementapi.services.IMembershipService;
 import com.project.managementapi.utils.Mapper;
 import com.project.managementapi.utils.Sports;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,9 @@ public class CustomerServiceImpl implements ICustomerService {
     @Autowired
     private PersonalInfoService personalInfoService;
 
+    @Autowired
+    private IMembershipService membershipService;
+
     @Override
     public CustomerDTO createCustomer(CustomerDTO customerDTO) {
 
@@ -29,6 +34,9 @@ public class CustomerServiceImpl implements ICustomerService {
                 .status(true)
                 .sports(Sports.valueOf(customerDTO.getSport()))
                 .build());
+
+        MembershipDTO membershipDTO = customerDTO.getMembershipDTO();
+        membershipService.createMembership(customer, membershipDTO);
 
         return Mapper.customerToDTO(customer);
     }
--- server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/MembershipServiceImpl.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/services/impl/MembershipServiceImpl.java
@@ -1,8 +1,25 @@
 package com.project.managementapi.services.impl;
 
+import com.project.managementapi.dtos.MembershipDTO;
+import com.project.managementapi.entities.Customer;
+import com.project.managementapi.entities.membership.EMembershipType;
+import com.project.managementapi.entities.membership.Membership;
+import com.project.managementapi.repositories.MembershipRepository;
 import com.project.managementapi.services.IMembershipService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MembershipServiceImpl implements IMembershipService {
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Override
+    public void createMembership(Customer customer, MembershipDTO membershipDto) {
+        membershipRepository.save(Membership.builder()
+                .membershipType(EMembershipType.valueOf(membershipDto.getMembershipType()))
+                .customer(customer)
+                .build());
+    }
 }
