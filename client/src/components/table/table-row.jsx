e8e892eb6b9eadce6cd65e4a5b2af7c0bb7e80de
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-22 11:16:00 -0300

    Fix: logic to make re-usuable

--- client/src/components/table/table-row.jsx
+++ client/src/components/table/table-row.jsx
@@ -1,15 +1,13 @@
-import { useNavigate } from "react-router-dom"
 
 
-function TableRow({ user, pathname }) {
-  
-  const navigate = useNavigate()
-  const setNavigatePath = pathname === "/" ? `/member/${user.dni}` : `${pathname}/${user.dni}`
+
+function TableRow({ data }) {
+
   return (
-    <tr onClick={() => navigate(setNavigatePath)} className="text-center [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0">
-      {Object.keys(user).map((key, index) => (
-        <td key={key} className={`custom-td ${index === 0 ? 'first:border-l' : ''} ${index === Object.keys(user).length - 1 ? 'last:border-r' : ''}`}>
-            {user[key]}
+    <tr className="text-center [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0">
+      {Object.keys(data).map((key, index) => (
+        <td key={key} className={`custom-td ${index === 0 ? 'first:border-l' : ''} ${index === Object.keys(data).length - 1 ? 'last:border-r' : ''}`}>
+            {data[key]}
         </td>
       ))}
     </tr>
