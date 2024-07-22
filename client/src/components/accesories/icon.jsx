01af671cf39603e27d2fb31aa9c3dc53c541884a
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-22 20:04:07 -0300

    Add: Plus Icon

--- client/src/components/accesories/icon.jsx
+++ client/src/components/accesories/icon.jsx
@@ -36,6 +36,10 @@ function Icon({ width = 32, height = width, fill = 'currentColor', iconName, cla
     mail_notifications: {
       path: `<path d="M120,128a32,32,0,1,0-32,32A32,32,0,0,0,120,128Zm-48,0a16,16,0,1,1,16,16A16,16,0,0,1,72,128Zm152-24H208V40a8,8,0,0,0-8-8H104a8,8,0,0,0-8,8V64H40A16,16,0,0,0,24,80v96a16,16,0,0,0,16,16H72v16a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V112A8,8,0,0,0,224,104Zm-58.34,60L216,127.65v72.7ZM112,48h80v77.24l-40,28.89V80a16,16,0,0,0-16-16H112ZM40,80h96v77.9c0,.12,0,.24,0,.36V176H40ZM88,192h48a16,16,0,0,0,16-16v-2.13L199.26,208H88Z"></path>`,
       viewBox: '0 0 256 256',
+    },
+    plus: {
+      path: `<path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>`,
+      viewBox: '0 0 256 256',
     }
 
   }
@@ -48,3 +52,4 @@ function Icon({ width = 32, height = width, fill = 'currentColor', iconName, cla
 }
 
 export default Icon
+
