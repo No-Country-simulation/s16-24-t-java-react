dfc73f1b099ffcf2ec60485b06eba73b0fc99ab6
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-23 18:15:11 -0300

    Fix: inner shadow style

--- client/src/components/table/filter.jsx
+++ client/src/components/table/filter.jsx
@@ -5,7 +5,7 @@ function Filter ({filters, handleChange}) {
 
   return (
     <>
-      <select className="px-6 py-2 bg-primary-20 text-white rounded-lg" name="filter" id="sport-selector" onChange={handleChange} disabled={filters.length < 1}>
+      <select className="px-6 py-2 bg-primary-20 text-white rounded-lg shadow-inner shadow-black" name="filter" id="sport-selector" onChange={handleChange} disabled={filters.length < 1}>
         <option className="text-white/70 "  value="all" selected>{t('filter.all')}</option>
         {filters && filters.map((filter) => <option key={filter} value={filter}>{t(`filter.${filter}`)}</option>)}
       </select>
--- client/src/components/table/search-input.jsx
+++ client/src/components/table/search-input.jsx
@@ -4,7 +4,7 @@ function SearchInput ({handleSearch}) {
 
 
   return (
-    <div className="flex border-primary-40 border-2 px-2  rounded-lg bg-primary-20 text-primary-10 items-center text-white">
+    <div className="flex border-primary-30 border-2 px-2  rounded-lg bg-primary-20 text-primary-10 items-center text-white shadow-inner shadow-black">
       <label htmlFor="search"><Icon iconName="magnifyingGlass" width="24"/></label>
       <input className="w-full bg-transparent focus:outline-none px-2 placeholder:text-white/80  font-semibold" type="text" id="search" name="search" onChange={handleSearch} placeholder="Ingresa un DNI"/>
     </div>
