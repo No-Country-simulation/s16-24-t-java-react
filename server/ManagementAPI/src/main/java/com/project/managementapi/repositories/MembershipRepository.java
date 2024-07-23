cbdd90a3f39f18cba3aa1f0ba8bc123417913fba
Leonel Amaya <omar.mejia2099@gmail.com>
2024-07-23 17:37:11 -0500

    add: membership update logic

--- server/ManagementAPI/src/main/java/com/project/managementapi/repositories/MembershipRepository.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/repositories/MembershipRepository.java
@@ -1,9 +1,11 @@
 package com.project.managementapi.repositories;
 
+import com.project.managementapi.entities.Customer;
 import com.project.managementapi.entities.membership.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Membership findByCustomer(Customer customer);
 }
