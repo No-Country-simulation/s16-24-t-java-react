f7a00d593057c784d4bd9c4ead68515d0cf97cad
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-24 19:15:25 -0300

    Fix: complex get and context provider

--- client/src/hooks/useGetComplex.jsx
+++ client/src/hooks/useGetComplex.jsx
@@ -1,19 +1,34 @@
 import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { LoginContext } from "../contexts/login-context";
 
-export const useGetComplex = async () => {
-  try { 
-    const { data } = await axios.get("/api/v1/complexes/findAll", {
-      headers: {
-        "Authorization": `Bearer ${localStorage.getItem("sportify_jwt_access")}`
+
+
+const useGetComplexes = () => {
+  const [complexes, setComplexes] = useState([]);
+  const [error, setError] = useState(null);
+  const { isLogged } = useContext(LoginContext);
+
+  useEffect(() => {
+    const GetAllComplexes = async () => {
+      try {
+        const { data } = await axios.get("/api/v1/complexes/findAll", {
+          headers: {
+            "Authorization": `Bearer ${localStorage.getItem("sportify_jwt_access")}`
+          }
+        });
+        const { object } = data
+        setComplexes(object.content);
+      } catch (error) {
+        setError(error);
+        setComplexes([]);
       }
-    });
-  
-    return data
-    
-  } catch (error) {
-    console.log(error);
-    return []
-  }
- 
-  // return data
+    }
+
+    GetAllComplexes()
+  }, [isLogged]);
+
+  return { complexes, error }
 }
+
+export default useGetComplexes
