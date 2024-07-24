7d2005816f76a169d8482907cc1f9f0835ed1970
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-24 13:48:30 -0300

    New: modal to-do to create complex and employee

---
+++ client/src/components/modals/create-complex.jsx
@@ -0,0 +1,17 @@
+import Modal from "./modal";
+
+function CreateComplex() {
+  return (
+    <Modal>
+       <div className="relative flex bg-gray-100 w-[800px] min-h-[200px] rounded-xl shadow-2xl flex-col p-10 gap-10 items-center" onClick={(e) => e.stopPropagation()} >
+          <p className="text-center text-2xl">Agregar un complejo</p>
+          <div className="flex gap-20">
+            <button className="bg-primary-20 text-secondary-10 font-bold px-20 py-3 rounded-full hover:bg-primary-0 hover:text-secondary-0">Si</button>
+            <button className="bg-secondary-20 text-primary-10 font-bold px-20 py-3 rounded-full hover:bg-secondary-0 hover:text-primary-0">No</button>
+          </div>
+       </div>
+    </Modal>
+  )
+}
+
+export default CreateComplex
