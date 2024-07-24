9e6f93459e505514a4b0990d63e654efcc8377e2
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-24 20:38:34 -0300

    Fix: format code

--- client/src/components/table/table-row.jsx
+++ client/src/components/table/table-row.jsx
@@ -7,12 +7,11 @@ function TableRow({ setUserID, handleProfileModal, data }) {
     handleProfileModal()
   }
 
-
   return (
     <tr onClick={handleClick} className="text-center even:bg-tertiary-0 odd:bg-tertiary-20 [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0">
       {Object.keys(data).map((key, index) => (
         <td key={key} className={`custom-td ${index === 0 ? 'first:border-l' : ''} ${index === Object.keys(data).length - 1 ? 'last:border-r' : ''}`}>
-            {data[key]}
+          {data[key]}
         </td>
       ))}
     </tr>
