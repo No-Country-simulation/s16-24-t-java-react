801944191c942df436ec96aee578634693f91623
Leonel Amaya <omar.mejia2099@gmail.com>
2024-07-24 15:09:08 -0500

    add: overlapping times validation exception

--- server/ManagementAPI/src/main/java/com/project/managementapi/repositories/WorkoutSessionRepository.java
+++ server/ManagementAPI/src/main/java/com/project/managementapi/repositories/WorkoutSessionRepository.java
@@ -2,11 +2,21 @@ package com.project.managementapi.repositories;
 
 import com.project.managementapi.entities.WorkoutSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface WorkoutSessionRepository extends JpaRepository<WorkoutSession, Long> {
     List<WorkoutSession> findByComplexId(Long id);
+    @Query("SELECT CASE WHEN COUNT(ws) > 0 THEN true ELSE false END " +
+            "FROM WorkoutSession ws " +
+            "WHERE ws.dayOfWeek = :dayOfWeek " +
+            "AND (:startTime < ws.endTime AND :endTime > ws.startTime)")
+    boolean existsOverlappingSession(@Param("dayOfWeek") Integer dayOfWeek,
+                                     @Param("startTime") LocalTime startTime,
+                                     @Param("endTime") LocalTime endTime);
 }
