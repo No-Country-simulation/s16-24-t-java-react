0f15e628c98aa94210436a46fbe59d97f676d52b
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-25 14:17:08 -0300

    New: modal create input

---
+++ client/src/components/accesories/input-create-modal.jsx
@@ -0,0 +1,10 @@
+function InputCreateModal({htmlFor, type, handleChange, label, labelClassName="text-primary-10 ml-5", inputClassName = "px-6 py-2 bg-primary-20 text-white rounded-full shadow-inner shadow-black border-2 border-primary-50", containerClassName="w-full flex flex-col gap-1"}) {
+  return (
+    <div className={containerClassName}>
+      <label className={labelClassName} htmlFor={htmlFor}>{label}</label>
+      <input className={inputClassName } onChange={handleChange} type={type} name={htmlFor} />
+    </div>
+  )
+}
+
+export default InputCreateModal
