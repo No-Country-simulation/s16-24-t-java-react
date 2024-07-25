15eb6a289566ba2d67114ff1092e33432376b7d1
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-25 16:16:39 -0300

    Fix: styles

--- client/src/components/table/table-header.jsx
+++ client/src/components/table/table-header.jsx
@@ -7,9 +7,9 @@ function TableHeader({headers}) {
 
     <>
       <thead className="text-center text-base bg-primary-10 text-white sticky top-0 left-0 border-x border-primary-10">
-        <tr className='[&>th]:py-4 [&>th]:px-6'>
+        <tr>
           {headers.map((header) => (
-            <th key={header}>{t(`tableHeader.${header}`)}</th>
+            <th className='py-4 px-6' key={header}>{t(`tableHeader.${header}`)}</th>
           ))}
         </tr>
       </thead>
