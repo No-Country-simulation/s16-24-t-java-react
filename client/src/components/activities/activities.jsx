828bcf8412d8853d1336964fff98d5cbfb725a9d
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-25 19:11:24 -0300

    Fix: logic to load activities

--- client/src/components/activities/activities.jsx
+++ client/src/components/activities/activities.jsx
@@ -1,27 +1,29 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { ComplexContext } from "../../contexts/complex-context";
 
 import Icon from "../accesories/icon";
-import { DaysColumns, Hours } from "../../lib/const";
 import AddActivity from "../modals/add-activity";
 import { getHourIndex } from "../../lib/helpers";
-import useGetActivities from "../../hooks/useGetActivities";
-
-
+import { DaysColumns, Hours } from "../../lib/const";
 
 function Calendar() {
 	const [showAddActivityModal, setShowAddActivityModal] = useState(false);
+  const [selectedComplex, setSelectedComplex] = useState([]);
 
 	const { t } = useTranslation();
 
-
-	// TODO - usar la api para traer todas las actividades
-	const {activities} = useGetActivities();
-	console.log("activities",activities);
+  const { complexes } = useContext(ComplexContext);
 
 	useEffect(() => {
+    console.log("complex", selectedComplex.activities);
 
-	},[activities]);
+	},[selectedComplex]);
+
+  const handleSelectComplex = (event) => {
+    const complexToSelect = complexes.find((complex) => complex.cuit === event.target.value);
+    setSelectedComplex(complexToSelect);
+  }
 
 	const handleAddModal = () => {
 		setShowAddActivityModal(!showAddActivityModal);
@@ -29,6 +31,11 @@ function Calendar() {
 
 	return (
 		<section className="h-full w-full">
+      <label className="mr-10" htmlFor="complex">Sleccionar sede:</label>
+      <select name="complex" id="" onChange={handleSelectComplex}>
+        <option className="text-center" value="" selected>sedes</option>
+        {complexes.map((complex) => <option key={complex.cuit} value={complex.cuit}>{complex.title}</option>)}
+      </select>
       <table className="w-full border-2 border-primary-0">
         <thead >
           <tr className="bg-primary-0 text-white">
@@ -43,7 +50,7 @@ function Calendar() {
             <tr key={rowIndex}>
               <td className="bg-tertiary-10 font-semibold text-center py-2">{hour}</td>
               {DaysColumns.map((_, colIndex) => {
-                const activity = activities.find(
+                const activity = selectedComplex.activities?.find(
                   (activity) =>
                     activity.day_of_week === colIndex + 1 &&
                     getHourIndex(activity.start_time) <= rowIndex &&
@@ -64,7 +71,7 @@ function Calendar() {
           ))}
         </tbody>
       </table>
-      {showAddActivityModal && <AddActivity handleAddModal={handleAddModal} />}
+      {showAddActivityModal && <AddActivity handleAddModal={handleAddModal} cuit={selectedComplex.cuit}/>}
     </section>
 	);
 }
