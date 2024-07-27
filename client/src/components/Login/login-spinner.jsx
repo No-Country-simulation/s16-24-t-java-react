26f3ef452bc01e1a8207c923d629f399534426dd
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-27 12:02:25 -0300

    Revert "Merge pull request #167 from No-Country-simulation/develop-front"
    This reverts commit 354178739a9236d29a8e04e46c115dd5ba74a40e, reversing
    changes made to a104a86e5daf30f651b8c4a9ab1c3755e07a56b6.

--- client/src/components/Login/login-spinner.jsx
+++ client/src/components/Login/login-spinner.jsx
@@ -2,8 +2,8 @@ import CircularProgress from '@mui/material/CircularProgress'
 
 function LoginSpinner() {
   return (
-    <div className='grid place-content-center min-h-dvh text-primary-20 items-center justify-center gap-10 w-full place-items-center bg-gradient-to-br from-primary-70 via-40% via-white to-secondary-70'>
-      <h1 className='text-4xl text-primary-40 font-semibold'>Iniciando Sesión...</h1>
+    <div className='grid place-content-center min-h-dvh text-primary-20 items-center justify-center gap-10 w-full place-items-center'>
+      <h1 className='text-4xl text-black '>Iniciando Sesión...</h1>
       <CircularProgress size={90} color='inherit'/>
     </div>
   )
