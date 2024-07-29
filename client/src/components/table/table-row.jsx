54e12b23356c7ae456791e6cc37c208170987ecd
Rodrigo Lezama <cordolezama@gmail.com>
2024-07-29 09:55:48 -0300

    Fix: styles when an employee or customer are down

--- client/src/components/table/table-row.jsx
+++ client/src/components/table/table-row.jsx
@@ -7,10 +7,17 @@ function TableRow({ setID, pathname, handleEditModal, data }) {
     handleEditModal()
   }
 
+  const handleStatus = () => {
+    if (pathname === PATHS.HEADQUARTERS) {
+      return true
+    }
+    return data.status
+  }
+
   return (
-    <tr onClick={handleClick} className="text-center even:bg-tertiary-0 odd:bg-tertiary-40 [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0 cursor-pointer hover:even:bg-secondary-70 odd:hover:bg-secondary-80">
+    <tr onClick={handleClick} className={`text-center ${handleStatus() ? `even:bg-tertiary-0 odd:bg-tertiary-40` : `bg-red-500`} [&>td]:py-4 [&>td]:border-b [&>td]:border-primary-0 cursor-pointer hover:even:bg-secondary-70 odd:hover:bg-secondary-80`}>
       {Object.keys(data).map((key) => (
-        <td key={key} className="first:border-l last:border-r">
+        <td key={key} className="first:border-l last:border-r ">
           {data[key]}
         </td>
       ))}
